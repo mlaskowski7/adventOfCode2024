8a1b49a18c7a4d1a17b82ac9fd86041552576b67
mlaskowski7 <mtlaskowski7@gmail.com>
2024-12-04 19:34:17 +0100

    solution for part 2 of 3rd day

--- day3/solution.py
+++ day3/solution.py
@@ -9,18 +9,32 @@ def handle_mul(mul):
 
     return int(numbers[0]) * int(numbers[1])
 
-def solve(input_text):
+def solve_first(input_text):
     valid_muls = re.findall(r'mul\(\d{1,3},\d{1,3}\)', input_text)
     total = 0
     for mul in valid_muls:
         total += handle_mul(mul)
     return total
 
+def solve_second(input_text):
+    instructions = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", input_text)
+    total = 0
+    enabled = True
 
+    for instruction in instructions:
+        if instruction == "do()":
+            enabled = True
+        elif instruction == "don't()":
+            enabled = False
+        elif enabled and instruction.startswith("mul("):
+            total += handle_mul(instruction)
+
+    return total
 
 if __name__ == '__main__':
     all_input = ""
     with open('input.txt', 'r') as input_file:
         for line in input_file:
             all_input += line
-    print(solve(all_input))
+    print(solve_first(all_input))
+    print(solve_second(all_input))
